2a6166ad3037df2cf16f377689a1969a1021875d
kardashevd <106695001+tsukedev@users.noreply.github.com>
2023-04-05 20:42:11 -0300

    updated Login structure with Header and better indentation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -7,21 +7,34 @@ const Login = () => {
 
   return (
     <>
-      <Header/>
+      <Header />
       <Container>
         <Section>
           <h1>Login</h1>
           <form>
             <div>
               <label htmlFor="username">Usuário</label>
-              <input type="text" id="username"  aria-label="E-mail" placeholder="Digitar usuário"/>
+              <input
+                type="text"
+                id="username"
+                aria-label="E-mail"
+                placeholder="Digitar usuário"
+              />
               <label htmlFor="password">Usuário</label>
-              <input type="text" id="password"  aria-label="Senha" placeholder="Digitar senha"/>              
+              <input
+                type="text"
+                id="password"
+                aria-label="Senha"
+                placeholder="Digitar senha"
+              />
             </div>
             <span>Esqueci minha senha</span>
-            <Button type="submit" children="Entrar"/>
+            <Button type="submit" children="Entrar" />
             <h3>Ainda não possui conta?</h3>
-            <SignInButton children="Cadastrar" onClick={() => navigate("/register")}/>
+            <SignInButton
+              children="Cadastrar"
+              onClick={() => navigate("/register")}
+            />
           </form>
         </Section>
       </Container>
