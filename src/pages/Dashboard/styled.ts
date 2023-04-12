344042b72e77ddedb4d0fb7666a9db04ba3d83fc
kardashevd <106695001+tsukedev@users.noreply.github.com>
2023-04-12 08:40:18 -0300

    add one photo. Ready.

--- src/pages/Dashboard/styled.ts
+++ src/pages/Dashboard/styled.ts
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 
-const ContainerDash = styled.div`
+export const Main = styled.main`
+  display: flex;
+	flex-direction: row;
+	width: 100%;
+	color: var(--fixed-white);
+	justify-content: space-between;
+	max-width: 1400px;
 
-`
 
-
-export default ContainerDash;
+  @media (max-width: 540px) {
+		display: flex;
+		flex-direction: column;
+  }
+`;
