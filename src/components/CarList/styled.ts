298cc33196956f55cbae446d94c8ba4f2930fdd7
kardashevd <106695001+tsukedev@users.noreply.github.com>
2023-04-12 10:47:25 -0300

    update CarList/styled.ts

--- src/components/CarList/styled.ts
+++ src/components/CarList/styled.ts
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const Section = styled.section`
 		display: flex;
+    flex-direction: row;
     flex-flow: row wrap;
     width: 80%;
     gap: 30px;
     max-width: 1012px;
-    flex-direction: row;
     flex-wrap: wrap;
     align-content: flex-start;
 		justify-content: center;
