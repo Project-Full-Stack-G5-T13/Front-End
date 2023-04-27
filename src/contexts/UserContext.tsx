fc6c20e549a3c70335b3865452023a1dd8f0861e
kardashevd <106695001+tsukedev@users.noreply.github.com>
2023-04-27 12:52:28 -0300

    fixed interface. Ready.

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -4,24 +4,96 @@ import { toast } from "react-toastify";
 import api from "../services/api";
 import jwtDecode from "jwt-decode";
 import axios from "axios";
-
 import { IUserUpdate } from "../components/ModalEditUser";
 import { IAddressUpdateRequest } from "../components/ModalEditAddress";
-
-import {
-  IFormLogin,
-  IFormSignup,
-  IJwtDecoded,
-  IProvidersProps,
-  IResetPassword,
-  ISendEmail,
-  IUser,
-  IUserContext,
-  IUserWithCars,
-} from "../interface/user/user.interface";
+import { IAdsReturn } from "../interface/card/card.interface";
+import { IResetPassword, ISendEmail } from "../interface/user/user.interface";
 
 export const UserContext = createContext({} as IUserContext);
 
+interface IProvidersProps {
+	children: ReactNode;
+}
+  
+interface IFormLogin {
+	email: string;
+	password: string;
+}
+  
+interface IFormSignup {
+	name: string;
+	email: string;
+	cpf: string;
+	phone_number: string;
+	birth_date: string;
+	description: string;
+	image_url?: string;
+	is_seller: boolean;
+	password: string;
+	confirmPassword: string;
+	address: {
+		zip_code: string;
+		state: string;
+		city: string;
+		street: string;
+		number: string;
+		complement: string;
+	};
+}
+  
+interface IUser {
+	id: string;
+	name: string;
+	email: string;
+	cpf: string;
+	phone_number: string;
+	birth_date: string;
+	description: string;
+	image_url?: string;
+	is_seller: boolean;
+	is_adm: boolean;
+	address: {
+		state: string;
+		city: string;
+		zip_code: string;
+		complement?: string;
+		number: string;
+		street: string;
+		id: string;
+	};
+}
+  
+interface IUserWithCars extends IUser {
+	cars: IAdsReturn[];
+  }
+  
+interface IJwtDecoded {
+	exp: number;
+	iat: number;
+	isAdm: boolean;
+	isSeller: boolean;
+	sub: string;
+}
+  
+interface IUserContext {
+	registerUser: (data: IFormSignup) => Promise<void>;
+	signInUser: (data: IFormLogin) => Promise<void>;
+	globalLoading: boolean;
+	setGlobalLoading: React.Dispatch<React.SetStateAction<boolean>>;
+	user: IUser | undefined;
+	setUser: React.Dispatch<React.SetStateAction<IUser | undefined>>;
+	getUser: () => Promise<void>;
+	userProfile: IUserWithCars;
+	getUserProfile(userId: string): Promise<void>;
+	updateUser: (
+	  data: IUserUpdate | IAddressUpdateRequest,
+	  closeModal: () => void
+	) => Promise<Omit<IUser, "address">>;
+	deleteUser: (closeModal: () => void) => Promise<void>;
+	sendEmail: (data: ISendEmail) => Promise<void>;
+	resetPassword: (data: IResetPassword, token: string) => Promise<void>;
+}
+  
 const Providers = ({ children }: IProvidersProps) => {
   const navigate = useNavigate();
 
