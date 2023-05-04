942f4cd206c957296f35295bc8dc20e5e5378eb8
m4chado <enzo653fm@gmail.com>
2023-05-04 12:20:22 -0300

    feat: ads complete info page. Working.

--- src/pages/Car/index.tsx
+++ src/pages/Car/index.tsx
@@ -1,113 +1,141 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { IoClose } from "react-icons/io5";
 import car from "../../assets/EXTERIOR-frontSidePilotNear-1653845164710-removebg-preview 1.png";
 import defaultphoto from "../../assets/default-user-image.png";
-import {
-	Article,
-	Aside,
-	Div,
-	Figure,
-	Main,
-	Modal,
-	SecondAside,
-	SecondDiv,
-	SecondMain,
-	Section,
-} from "./styled";
+import { Container, Main, Section, UserImage } from "./styled";
 import Comments from "../../components/Comments";
 import MakeComment from "../../components/MakeComment";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AdsContext } from "../../contexts/AdsContext";
+import { IAdsCompleteReturn } from "../../interface/card/card.interface";
+import UserAvatar from "../../components/UserAvatar";
+import { StyledButton_black, StyledButton_primary } from "../../styles/buttons";
+import { StyledHeading_7_500, StyledSpanDetail } from "../../styles/typografy";
+import SliderModal from "../../components/SliderModal";
+import { PointSpreadLoading } from "react-loading-typescript";
+import MakeComments from "../../components/MakeComment";
 
 function Car() {
+	const { getCar } = useContext(AdsContext);
+	const [currentCar, setCurrentCar] = useState<IAdsCompleteReturn | undefined>();
+	const [data, setData] = useState<any>({});
+	const [images, setImages] = useState([]);
+	const { id } = useParams();
 	const [isOpen, setIsOpen] = useState(false);
 
-	const navigate = useNavigate();
+	useEffect(() => {
+		const fetchCar = async () => {
+			const car = await getCar(id);
+			setCurrentCar(car);
+		};
+		fetchCar();
+	}, [id]);
+
+	useEffect(() => {
+		setImages([]);
+		if (currentCar) {
+			setImages([
+				currentCar.images.main_image,
+				currentCar.images.image_one,
+				currentCar.images.image_three,
+			]);
+			if (currentCar.images.image_four) {
+				setImages([...images, currentCar.images.image_four]);
+			}
+			if (currentCar.images.image_five) {
+				setImages([...images, currentCar.images.image_five]);
+			}
+		}
+	}, [currentCar]);
+
+	const handleOpen = (firstImage: number) => {
+		setIsOpen(true);
+		setData({
+			images,
+			firstImage,
+			model: currentCar.model,
+		});
+	};
+
+	const handleClose = () => {
+		setIsOpen(false);
+	};
 
 	return (
 		<>
 			<Section>
-				<Article>
-					<Main>
-						<Figure onClick={() => setIsOpen(true)}>
-							<img src={car} alt="" />
-						</Figure>
-						<Div>
-							<h3>Mercedes Benz A 2000 CGI ADVANCE SEDAN Mercedes Benz A 200</h3>
-							<div className="container">
-								<div className="priceAndSpan">
-									<div className="span">
-										<span>2013</span>
-										<span>0 KM</span>
+				<Container>
+					{currentCar ? (
+						<>
+							<Main>
+								<div className="main-content">
+									<div className="main-picture">
+										<img
+											src={currentCar.images.main_image}
+											alt="main image"
+											onClick={() => handleOpen(0)}
+										/>
 									</div>
-									<div className="price">
-										<p>R$ 40.000,00</p>
+									<div className="car-info-box">
+										<h3>{currentCar.model}</h3>
+										<div className="car-info">
+											<div className="info">
+												<StyledSpanDetail>
+													{currentCar.launch_year}
+												</StyledSpanDetail>
+												<StyledSpanDetail>
+													{currentCar.km} KM
+												</StyledSpanDetail>
+											</div>
+
+											<StyledHeading_7_500>
+												{currentCar.price.toLocaleString("pt-br", {
+													style: "currency",
+													currency: "BRL",
+												})}
+											</StyledHeading_7_500>
+										</div>
+										<StyledButton_primary>Comprar</StyledButton_primary>
+									</div>
+									<div className="car-description-box">
+										<h3>Descrição</h3>
+										<p>{currentCar.description}</p>
 									</div>
 								</div>
-								<button>Comprar</button>
-							</div>
-						</Div>
-						<SecondDiv className="description">
-							<h3>Descrição</h3>
-							<p>
-								Lorem Ipsum is simply dummy text of the printing and typesetting
-								industry. Lorem Ipsum has been the industry's standard dummy text
-								ever since the 1500s, when an unknown printer took a galley of type
-								and scrambled it to make a type specimen book.
-							</p>
-						</SecondDiv>
-					</Main>
-					<SecondMain>
-						<Aside>
-							<div>
-								<h3>Fotos</h3>
-							</div>
-							<div className="aside-images">
-								<img src={car} alt="" />
-								<img src={car} alt="" />
-								<img src={car} alt="" />
-								<img src={car} alt="" />
-								<img src={car} alt="" />
-								<img src={car} alt="" />
-							</div>
-						</Aside>
-						<SecondAside>
-							<img src={defaultphoto} alt="" />
-							<h3>Samuel Leão</h3>
-							<p>
-								Lorem Ipsum is simply dummy text of the printing and typesetting
-								industry. Lorem Ipsum has been the industry's standard dummy text
-								ever since the 1500s
-							</p>
-							<button>Ver todos anúncios</button>
-						</SecondAside>
-					</SecondMain>
-				</Article>
-				<Comments />
-				<MakeComment />
+								<div className="side-content">
+									<div className="car-gallery">
+										<h3>Fotos</h3>
+										<div className="wrapper">
+											{images.map((i, index) => (
+												<div key={index} className="img-box">
+													<img
+														src={i}
+														onClick={() => handleOpen(index)}
+													/>
+												</div>
+											))}
+										</div>
+									</div>
+									<div className="user-box">
+										<UserImage>
+											<div>{currentCar.user.name[0].toUpperCase()}</div>
+										</UserImage>
+										<h2>{currentCar.user.name}</h2>
+										<StyledButton_black>
+											Ver todos os anuncios{" "}
+										</StyledButton_black>
+									</div>
+								</div>
+							</Main>
+							<Comments />
+							<MakeComments />
+						</>
+					) : (
+						<div>car not found</div>
+					)}
+				</Container>
 			</Section>
-			{isOpen && (
-				<Modal
-					id="overlay"
-					onClick={(e: React.MouseEvent<HTMLDivElement>) => {
-						const target = e.target as HTMLButtonElement;
-						if (target.id === "overlay") {
-							setIsOpen(false);
-						}
-					}}
-				>
-					<div className="modal-wrapper">
-						<div className="modal-header">
-							<h3>Mercedes Benz A 2000 CGI ADVANCE SEDAN</h3>
-							<button onClick={() => setIsOpen(false)}>
-								<IoClose />
-							</button>
-						</div>
-						<div className="modal-img">
-							<img src={car} alt="" />
-						</div>
-					</div>
-				</Modal>
-			)}
+			{isOpen && <SliderModal onClose={handleClose} data={data} />}
 		</>
 	);
 }
