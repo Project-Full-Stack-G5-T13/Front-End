7bc2b427424a87fcfcaa4ef7092b4912b5b26bdf
ViniciusQuirino <viniciusquirino13@hotmail.com>
2023-05-09 11:52:58 -0300

    correction modal

--- src/pages/Profile/style.ts
+++ src/pages/Profile/style.ts
@@ -6,7 +6,6 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-
   padding: 1rem;
   min-height: 100vh;
   background-image: url(${halfimg});
