22420f916835812427a32557ad6ecfb6eb20aaa6
m4chado <enzo653fm@gmail.com>
2023-05-11 14:25:48 -0300

    Feat: pagination on dashboard. Working.

---
+++ src/components/Pagination/style.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const StyledPagination = styled.div`
+	width: 100%;
+	min-height: 10px;
+	border: 1px solid red;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 1rem;
+
+	.pagination-box {
+		display: flex;
+		gap: 1rem;
+		align-items: center;
+	}
+
+	.current-page {
+		border: none;
+		font-size: large;
+		font-weight: bold;
+		color: var(--grey-2);
+	}
+	.total-page {
+		border: none;
+		font-size: large;
+		font-weight: bold;
+		color: var(--grey-3);
+	}
+
+	.page-btn {
+		border: none;
+		font-size: large;
+		font-weight: bold;
+		color: var(--brand-1);
+	}
+
+	.page-btn:disabled {
+		opacity: 0.5;
+	}
+`;
